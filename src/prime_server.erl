6dd85a1a6cdc14fc8a9e2d208289c8524c4ce8a5
Jamie Clinton <jcclinton24@gmail.com>
2014-04-24 12:48:48 -0700

    added function to count number of prime checking children

--- src/prime_server.erl
+++ src/prime_server.erl
@@ -12,6 +12,13 @@
 -export([start_link/0]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, code_change/3, terminate/2]).
 
+-export([get_count/0]).
+
+
+
+get_count() ->
+	supervisor:count_children(prime_check_sup).
+
 
 
 
